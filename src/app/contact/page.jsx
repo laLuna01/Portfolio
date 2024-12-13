4218fd9e2001b4b22aab34742244b92ac72c70b7
Luana-SMatos <luana.smatos01@gmail.com>
2024-12-13 18:54:36 -0300

    contact page update

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -10,7 +10,7 @@ import { motion } from "framer-motion";
 const info = [
   {
     icon: <FaPhoneAlt />,
-    title: "Phone",
+    title: "Telefone",
     description: "(+55) 11 98516-5812",
   },
   {
@@ -20,7 +20,7 @@ const info = [
   },
   {
     icon: <FaMapMarkedAlt />,
-    title: "Address",
+    title: "Endereço",
     description: "Zona Sul - São Paulo, Brasil",
   },
 ]
@@ -31,29 +31,29 @@ const Contact = () => {
       <div className="flex flex-col xl:flex-row gap-[30px]">
         <div className="xl:w-[54%] order-2 xl:order-none">
           <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-            <h3 className="text-4xl text-accent">Let&apos;s work together</h3>
-            <p className="text-white/60">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque autem quidem, sed ea aliquid animi consequatur optio, fugiat accusamus porro assumenda. Esse impedit, eum sit laudantium reprehenderit ipsum vel quod.</p>
+            <h3 className="text-4xl text-accent">Vamos trabalhar juntos</h3>
+            <p className="text-white/60">Se você tem uma ideia, um projeto ou apenas quer trocar uma ideia, estou aqui para ouvir! Entre em contato através de um dos canais disponíveis e vamos conversar.</p>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-              <Input type="firstname" placeholder="First Name" />
-              <Input type="lastname" placeholder="Last Name" />
-              <Input type="email" placeholder="Email Address" />
-              <Input type="phone" placeholder="Phone Number" />
+              <Input type="firstname" placeholder="Nome" />
+              <Input type="lastname" placeholder="Sobrenome" />
+              <Input type="email" placeholder="Email" />
+              <Input type="phone" placeholder="Telefone" />
             </div>
             <Select>
               <SelectTrigger className="w-full">
-                <SelectValue placeholder="Select a service" />
+                <SelectValue placeholder="Selecione um serviço" />
               </SelectTrigger>
               <SelectContent>
                 <SelectGroup>
-                  <SelectLabel>Select a service</SelectLabel>
+                  <SelectLabel>Selecione um serviço</SelectLabel>
                   <SelectItem value="est">Web Development</SelectItem>
                   <SelectItem value="cst">UI/UX Design</SelectItem>
                   <SelectItem value="mst">Logo Design</SelectItem>
                 </SelectGroup>
               </SelectContent>
             </Select>
-            <Textarea className="h-[200px]" placeholder="Type your message here" />
-            <Button size="md" className="max-w-40 bg-accent hover:bg-accent-hover">Send Message</Button>
+            <Textarea className="h-[200px]" placeholder="Escreva sua mensagem aqui" />
+            <Button size="md" className="max-w-40 bg-accent hover:bg-accent-hover">Enviar</Button>
           </form>
         </div>
         <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
